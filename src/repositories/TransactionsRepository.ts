df8f0d2d7702b6ae85d243d3c213140dccbcf198
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-04-21 16:58:34 -0300

    challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,27 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length === 0) {
+      return { income: 0, outcome: 0, total: 0 };
+    }
+    const income = this.transactions
+      .map(e => (e.type === 'income' ? e.value : 0))
+      .reduce((first, next) => first + next);
+
+    const outcome = this.transactions
+      .map(e => (e.type === 'outcome' ? e.value : 0))
+      .reduce((first, next) => first + next);
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Transaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: RequestDTO): Transaction | string {
+    const { total } = this.transactionsRepository.getBalance();
+    const transaction = (): Transaction =>
+      this.transactionsRepository.create(
+        new Transaction({
+          title,
+          value,
+          type,
+        }),
+      );
+    if (type === 'income') {
+      return transaction();
+    }
+    if (type === 'outcome' && total > value) {
+      return transaction();
+    }
+    throw Error('The value costs more than total or the type is wrong');
   }
 }
 
